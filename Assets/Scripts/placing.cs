99d571acac9b403b602819f86809a49ae68e9924
Jay Sharma <sharmajay110601@gmail.com>
2021-03-29 14:27:39 +0530

    FIrst Commit
    Added Basic mechanics

---
+++ Assets/Scripts/placing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class placing : MonoBehaviour
+{
+    public Sprite furrow1;
+    BoxCollider2D m_Collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        if(main.currenttool == "furrow")
+        {
+            Debug.Log("Furrow picked");
+            GetComponent<SpriteRenderer>().sprite = furrow1;
+            transform.rotation = Quaternion.Euler(Vector3.forward * -25);
+            m_Collider = GetComponent<BoxCollider2D>();
+            m_Collider.offset =  new Vector2(0.04244328f,0.007222198f);
+            m_Collider.size =  new Vector2(2.423902f,0.4296626f);
+        }
+    } 
+}
---
+++ Assets/Scripts/points.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class points : MonoBehaviour
+{
+    public Text thisText;
+    public int score;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisText = GetComponent<Text>();
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void addscore()
+    {
+        score += 100;
+        thisText.text = "" + score;
+    }
+
+    public void subtractscore()
+    {
+        score -= 10;
+        thisText.text = "" + score;
+    }
+}
